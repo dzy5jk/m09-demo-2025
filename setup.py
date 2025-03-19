ce21d211a6f38d8d2cb3af5ef0fb5a84827fbcb5
Ryan Adoremos <dzy5jk@virginia.edu>
2025-03-19 10:54:25 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/dzy5jk/m09-demo-2025',
+    author='Ryan Adoremos',
+    author_email='dzy5jk@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
+
---
+++ test_script.py
@@ -0,0 +1,4 @@
+from demo.jabber import tell_me_something
+
+tell_me_something()
+
